c7081ab69ff710ec87dac6d5e011e56bb1d52fa9
Rogerio Angeliski <angeliski@hotmail.com>
2019-10-15 11:37:39 -0300

    Ajustando detalhes do card de eventos

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -2,7 +2,7 @@
   <b-card
     :header="event.title"
     header-tag="h1"
-    :footer="`Autor: ${event.autor}`"
+    :footer="`Localização: ${event.local}`"
     :sub-title="event.tipoDoEvento"
     :img-src="event.imagem"
     class="mt-1 text-center"
@@ -11,8 +11,8 @@
     header-text-variant="white"
   >
     <b-card-text>Data: {{event.data}}</b-card-text>
-    <b-card-text>Horário: {{event.horarioInicio}} – {{event.horarioFim}}</b-card-text>
-    <b-card-text>Localização: {{event.local}}</b-card-text>
+    <b-card-text>Horário: {{event.horarioInicio}} <span v-if="event.horarioFim">– {{event.horarioFim}}</span></b-card-text>
+    <b-card-text></b-card-text>
     <b-card-text><span v-html="event.descricao"></span></b-card-text>
     <a :href="event.link" target="_blank" class="card-link">Acesse Aqui</a>
   </b-card>
